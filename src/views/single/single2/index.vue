cdf050f4c6d6f040979444f88200fc38fd2969b4
zmx2321 <zmx2321@163.com>
2024-07-18 16:36:35 +0800

    fix: init

--- src/views/single/single2/index.vue
+++ src/views/single/single2/index.vue
@@ -1,10 +1,47 @@
 <template>
   <section>
-    single2
+    <upload-tool @verifyFileType="verifyFileType" @getFile="getFile">
+      <template #tip>
+        <span>请上传xls或者xlsx格式</span>
+      </template>
+    </upload-tool>
+
+    <div class="detail_wrap">
+    </div>
   </section>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import * as XLSX from 'xlsx';
+
+const excelResData = ref([])
+
+const verifyFileType = (file, next) => {
+  if (!/\.(xls|xlsx)$/.test(file.name.toLowerCase())) {
+    ElMessage({
+      message: "上传格式不正确，请上传xls或者xlsx格式！",
+      type: "error",
+    });
+    next(false)
+    return
+  }
+  next(true)
+}
+
+const getFile = async (file) => {
+  console.log(file)
+};
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.detail_wrap {
+  padding: 20px;
+  width: 95%;
+  height: 80vh;
+  border: solid 1px #ccc;
+  margin: 0 auto;
+  overflow: auto;
+}
+</style>
